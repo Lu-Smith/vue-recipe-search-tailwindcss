440cc9838c4d80bab0a926a6cebb276cfc6247f3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-04 18:52:41 +0000

    Updated Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,13 @@
 <template>
-    <h3>Footer</h3>
+      <h3 class="flex items-start justify-center gap-1 text-sm mt-2">
+        Coded by 
+        <a 
+        href="https://www.lunasmithart.com/" 
+        target="_blank"
+        class="text-linksColor hover:text-orange-400">Luna Smith</a>-
+        <a 
+        href="https://github.com/Lu-Smith/vue-recipe-search-tailwindcss" 
+        target="_blank"
+        class="text-linksColor hover:text-orange-400">GitHub</a>.
+    </h3>
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,12 @@
 module.exports = {
   content: [ 
     "./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
+    theme: {
+      extend: {
+        colors: {
+          linksColor: '#f95959', 
+        },
+      },
+    },
   plugins: [],
 }
