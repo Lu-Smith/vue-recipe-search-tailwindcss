de3f1f6ce1a7cf4bcdc64fae20abc5c03fed65a9
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-04 18:44:50 +0000

    Deleted HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+
 </template>
 
-<style scoped>
+<script setup lang="ts">
 
-</style>
+</script>
+
--- src/components/HelloWorld.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h1>Hello world</h1>
-  <p class="text-green-400 items-center bg-red-300">Hello</p>
-</template>
-
