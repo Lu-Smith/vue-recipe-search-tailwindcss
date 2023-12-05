e273b257589275aadc0ef6ad869555b57b5b13f0
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-05 17:07:02 +0000

    Updated Home.vue

--- src/router/views/Home.vue
+++ src/router/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
-    <input type="text" />
+    <div class="flex p-8 justify-center">
+        <input 
+        type="text"
+        class="rounded bordr-2 border-grey-200 w-full"
+        placeholder="Search for meals..." />
+        <div 
+        class="flex gap-1">
+        </div>
+    </div>
 </template>
 
 <script lang="ts" setup>
