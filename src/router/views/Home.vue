36b23f9ba4ee12ad930d5e24ca6573d0425b2a32
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-06 15:00:23 +0000

    Updated index.js

--- src/router/views/Home.vue
+++ src/router/views/Home.vue
@@ -1,23 +1,22 @@
 <template>
     <div class="flex p-8 justify-center items-center flex-col gap-2">
-        <input 
+      <input
         type="text"
-        class="rounded bordr-2 border-grey-200 w-full"
-        placeholder="Search for meals..." />
-        <div 
-        class="flex gap-1">
+        class="rounded border-2 border-grey-200 w-full"
+        placeholder="Search for meals..."
+      />
+      <div class="flex gap-2">
         <router-link to="/" v-for="letter of letters">
-            {{  letter }}
+          <span class="hover:text-linksColor duration-300">{{ letter }}</span>
         </router-link>
-        </div>
+      </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
-import store from '../store';
+    import { computed } from 'vue';
+    import store from '../store';
 
-const meals = computed(() => store.state.meals);
-const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-
+    const meals = computed(() => store.state.meals);
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 </script>
