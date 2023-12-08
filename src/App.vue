beca76cd51a112d7ac54345f8faf3421d154e388
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-08 18:54:41 +0000

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,29 @@
   <div class="bg-bgColor h-100">
     <header class="bg-textColor text-bgColor 
     shadow-md h-16 flex justify-between items-stretch">
-      <router-link :to="{name: 'home'}">
+      <router-link 
+      :to="{name: 'home'}"
+      class="px-2 h-full hover:text-linksColor font-bold text-xl ml-2
+        duration-600 inline-flex items-center transition-colors">
         Home
       </router-link>
       <div class="flex items-center">
         <router-link 
         :to="{name: 'home'}" 
         class="px-2 h-full hover:bg-linksColor hover:text-textColor
-        duration-300 inline-flex items-center">
+        duration-600 inline-flex items-center transition-colors  font-medium">
           Search Meals
         </router-link>
         <router-link 
         :to="{name: 'home'}"
         class="px-2 h-full hover:bg-linksColor hover:text-textColor
-        duration-300 inline-flex items-center">
+        duration-600 inline-flex items-center transition-colors  font-medium">
           Search by Letter
         </router-link>
         <router-link 
         :to="{name: 'home'}"
         class="px-2 h-full hover:bg-linksColor hover:text-textColor
-        duration-300 inline-flex items-center">
+        duration-600 inline-flex items-center transition-colors font-medium pr-4">
           Search by Ingredients
         </router-link>
       </div>
