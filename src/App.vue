7dda431a399abb226dd3cfd8163c86000579f4cd
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-08 18:25:38 +0000

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="bg-addColor h-100">
-    <header class="bg-textColor shadow-md h-24">
+  <div class="bg-bgColor h-100">
+    <header class="bg-textColor shadow-md h-16 flex justify-between items-center">
+      <router-link :to="{name: 'home'}">
+        Home
+      </router-link>
+      <div>
+        <router-link :to="{name: 'home'}">
+          Search Meals
+        </router-link>
+        <router-link :to="{name: 'home'}">
+          Search by Letter
+        </router-link>
+        <router-link :to="{name: 'home'}">
+          Search by Ingredients
+        </router-link>
+      </div>
     </header>
     <main>
       <router-view />
