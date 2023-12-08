d4af7275522f33e35a8b428a3112585547453453
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-08 19:52:58 +0000

    Updated DefaultLayout
    Adjusted App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,4 @@
 <template>
-  <div class="bg-bgColor h-100">
-    <NavBar />
-    <main>
-      <router-view />
-    </main>
-    <Footer />
-  </div>
+ <router-view />
 </template>
-
-<script setup lang="ts">
-  import Footer from './components/Footer.vue';
-  import NavBar from './components/NavBar.vue';
-</script>
 
--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -1,3 +1,15 @@
 <template>
-    
+     <div class="bg-bgColor h-100">
+    <NavBar />
+    <main>
+      <router-view />
+    </main>
+    <Footer />
+  </div>
 </template>
+
+<script setup lang="ts">
+  import Footer from './Footer.vue';
+  import NavBar from './NavBar.vue';
+</script>
+
