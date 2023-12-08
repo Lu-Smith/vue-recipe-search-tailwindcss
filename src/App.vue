e9cf01d8276e88777f31806a580254b89916d11d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-08 19:43:49 +0000

    Created NavBar and added to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,6 @@
 <template>
   <div class="bg-bgColor h-100">
-    <header class="bg-textColor text-bgColor 
-    shadow-md h-16 flex justify-between items-stretch">
-      <router-link 
-      :to="{name: 'home'}"
-      class="px-2 h-full hover:text-linksColor font-bold text-xl ml-2
-        duration-600 inline-flex items-center transition-colors">
-        Home
-      </router-link>
-      <div class="flex items-center">
-        <router-link 
-        :to="{name: 'byMeal'}" 
-        class="px-2 h-full hover:bg-linksColor hover:text-textColor
-        duration-600 inline-flex items-center transition-colors  font-medium">
-          Search Meals
-        </router-link>
-        <router-link 
-        :to="{name: 'byLetter'}"
-        class="px-2 h-full hover:bg-linksColor hover:text-textColor
-        duration-600 inline-flex items-center transition-colors  font-medium">
-          Search by Letter
-        </router-link>
-        <router-link 
-        :to="{name: 'byIngredients'}"
-        class="px-2 h-full hover:bg-linksColor hover:text-textColor
-        duration-600 inline-flex items-center transition-colors font-medium pr-4">
-          Search by Ingredients
-        </router-link>
-      </div>
-    </header>
+    <NavBar />
     <main>
       <router-view />
     </main>
@@ -38,5 +10,6 @@
 
 <script setup lang="ts">
   import Footer from './components/Footer.vue';
+  import NavBar from './components/NavBar.vue';
 </script>
 
---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="bg-textColor text-bgColor 
+    shadow-md h-16 flex justify-between items-stretch">
+    <router-link 
+    :to="{name: 'home'}"
+    class="px-2 h-full hover:text-linksColor font-bold text-xl ml-2
+        duration-600 inline-flex items-center transition-colors">
+        Home
+    </router-link>
+    <div class="flex items-center">
+        <router-link 
+        :to="{name: 'byMeal'}" 
+        class="px-2 h-full hover:bg-linksColor hover:text-textColor
+        duration-600 inline-flex items-center transition-colors  font-medium">
+        Search Meals
+        </router-link>
+        <router-link 
+        :to="{name: 'byLetter'}"
+        class="px-2 h-full hover:bg-linksColor hover:text-textColor
+        duration-600 inline-flex items-center transition-colors  font-medium">
+        Search by Letter
+        </router-link>
+        <router-link 
+        :to="{name: 'byIngredients'}"
+        class="px-2 h-full hover:bg-linksColor hover:text-textColor
+        duration-600 inline-flex items-center transition-colors font-medium pr-4">
+        Search by Ingredients
+        </router-link>
+    </div>
+    </header>
+</template>
