aa68b19dac90faf2103ee3f8c039f43f622ddd2f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-09 20:05:01 +0000

    Updated SearchByMeal.vue

--- src/router/views/SearchByMeal.vue
+++ src/router/views/SearchByMeal.vue
@@ -1,7 +1,13 @@
 <template>
+    <div class="p-8">
         <input
         type="text"
         class="rounded border-2 border-grey-200 w-full"
         placeholder="Search for meals..."
-      />
+        />
+    </div>
 </template>
+
+<script setup lang="ts">
+
+</script>
