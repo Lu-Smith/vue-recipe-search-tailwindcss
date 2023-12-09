922ab39b9fd855a5c5b1a22a2390c4d385728a65
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-09 19:58:46 +0000

    Updated SearchByLetters.vue

--- src/router/views/Home.vue
+++ src/router/views/Home.vue
@@ -1,13 +1,6 @@
 <template>
     <div class="flex p-8 justify-center items-center flex-col gap-2">
-      <div class="flex gap-2">
-        <router-link 
-        :to="{name: 'byLetter', params: {letter}}" 
-        v-for="letter of letters"
-        :key="letter">
-          <span class="hover:text-linksColor duration-300">{{ letter }}</span>
-        </router-link>
-      </div>
+
       <!-- <pre>{{ ingredients }}</pre> -->
     </div>
 </template>
--- src/router/views/SearchByLetter.vue
+++ src/router/views/SearchByLetter.vue
@@ -1,3 +1,28 @@
 <template>
-    <h1>Search by Letter</h1>
+    <div>
+        <div class="flex gap-2">
+        <router-link 
+        :to="{name: 'byLetter', params: {letter}}" 
+        v-for="letter of letters"
+        :key="letter">
+          <span class="hover:text-linksColor duration-300">{{ letter }}</span>
+        </router-link>
+      </div>
+    </div>
 </template>
+
+<script lang="ts" setup>
+    import { onMounted, ref } from 'vue';
+    import axiosClient from '../../axiosClient.js';
+
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    const ingredients = ref([])
+
+    onMounted(async () => {
+      const response = await axiosClient.get('list.php?i=list')
+
+      ingredients.value = response.data
+  
+      console.log (ingredients.value)
+    })
+</script>
