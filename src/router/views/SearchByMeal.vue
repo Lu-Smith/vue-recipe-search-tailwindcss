3ef7992d5214798a246d8e66a3b09d2538ee4505
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-10 17:08:56 +0000

    Updated actions.js

--- src/router/views/SearchByMeal.vue
+++ src/router/views/SearchByMeal.vue
@@ -4,10 +4,17 @@
         type="text"
         class="rounded border-2 border-grey-200 w-full"
         placeholder="Search for meals..."
+        @change="searchMeals"
         />
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const keyword = ref('');
+function SearchMeals() {
+    axiosClient.get(`search.php?s=${keyword.value}`)
+};
 
 </script>
