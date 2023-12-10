2ae89cd0b827837b146fe94f03cbaf36ff953cd1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-10 18:49:54 +0000

    Updated Typescript actions.ts

--- src/router/store/actions.ts
+++ src/router/store/actions.ts
@@ -1,8 +1,12 @@
 import axiosClient from '../../axiosClient';
+import { Commit } from 'vuex';
 
-export function searchMeals({commit}, keyword) {
+export function searchMeals({commit}: { commit: Commit }, keyword: string) {
     axiosClient.get(`search.php?s=${keyword}`)
     .then(({data}) => {
         commit('setSearchedMeals', data.meals)
     })
+    .catch((error) => {
+        console.error(error);
+    });
 }
