5f0dcea85c708296c67ae691615a897176ce2de4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-12 18:21:03 +0000

    Updated SearchByMeal.vue

--- src/router/views/SearchByMeal.vue
+++ src/router/views/SearchByMeal.vue
@@ -1,28 +1,44 @@
 <template>
     <div class="p-8">
-        <input
+      <input
         type="text"
         class="rounded border-2 border-grey-200 w-full text-bgColor"
         placeholder="Search for meals..."
         @change="searchMeals"
         v-model="keyword"
-        />
+      />
     </div>
-    <div>
-        <pre>{{ meals }}</pre>
-        <pre>{{ keyword }}</pre>
+    <div class="grid grid-cols-2 gap-3 md:grid-cols-3">
+      <div v-for="meal of meals" :key="meal.idMeal" >
+        <img :src="meal.strMealThumb" :alt="meal.strMeal">
+        <h3>{{ meal.strMeal }}</h3>
+        <div>
+          <a :href="meal.strYoutube" target="_blank">YouTube</a>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import store from '../store'
+import { ref, computed, onMounted } from 'vue';
+import store from '../store';
+
+interface MealsProps {
+      strYoutube: string;
+      strMeal: string;
+      strMealThumb: string;
+      idMeal: string;
+    }
 
 const keyword = ref('');
-const meals = computed(() => store.state.searchedMeals)
+const meals = computed<MealsProps[]>(() => store.state.searchedMeals)
 function searchMeals() {
     store.dispatch('searchMeals', keyword.value)
-    console.log(keyword.value)
+    
 };
+
+onMounted(() => {
+  console.log(meals.value);
+});
 
 </script>
