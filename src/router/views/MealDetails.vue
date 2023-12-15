a4add8e75aa6bac2c474a2d78df5d6c9b53e8325
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-15 22:30:33 +0000

    Updated MealDeatails.vue

--- src/router/views/MealDetails.vue
+++ src/router/views/MealDetails.vue
@@ -1,3 +1,12 @@
 <template>
-    Hello
+    <div>
+        <pre>{{ meal }}</pre>
+    </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const meal = ref({})
+
+</script>
