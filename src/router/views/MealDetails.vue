4c1cb455ce5ecc5a1216f8a5df127ece5b624cec
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-16 23:47:25 +0000

    Updated styling in MealDetails.vue

--- src/router/views/MealDetails.vue
+++ src/router/views/MealDetails.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tracking-wide w-[800px] mx-auto">
+    <div class="tracking-wide mx-auto">
         <h1 class="text-2xl font-bold m-5">{{ meal.strMeal }}</h1>
         <img :src="meal.strMealThumb" :alt="meal.strMeal">
         <div class="grid grid-cols-1 md:grid-cols-3">
@@ -14,22 +14,24 @@
             </div>
         </div>
         <div class="mt-5">
-            <h2 class="text-2xl font-semibold mb-2">Ingredients and Measures</h2>
-            <div class="w-[800px]">
-                <table class="border border-textColor">
+            <h2 class="text-2xl font-semibold mb-2 text-center">Ingredients and Measures</h2>
+            <div class="mx-auto">
+                <table class="border border-addColor mx-auto">
                     <thead>
-                        <tr class="bg-addColor">
-                        <th class="py-2 px-4 border-b ">Index</th>
-                        <th class="py-2 px-4 border-b text-right">Ingredients</th>
-                        <th class="py-2 px-4 border-b text-left">Measures</th>
+                        <tr class="bg-slate-800">
+                        <th class="py-2 px-4 border-b border-addColor">Index</th>
+                        <th class="py-2 px-4 border-b border-addColor">Ingredients</th>
+                        <th class="py-2 px-4 border-b border-addColor text-left">Measures</th>
                         </tr>
                     </thead>
                     <tbody>
                         <template v-for="(_, index) of new Array(20)">
-                        <tr v-if="meal[`strIngredient${index + 1}`] || meal[`strMeasure${index + 1}`]">
-                            <td class="py-2 px-4 border-b text-center">{{ index + 1 }}</td>
-                            <td class="py-2 px-4 border-b text-right">{{ meal[`strIngredient${index + 1}`] }}</td>
-                            <td class="py-2 px-4 border-b text-left">{{ meal[`strMeasure${index + 1}`] }}</td>
+                        <tr 
+                        v-if="meal[`strIngredient${index + 1}`] || meal[`strMeasure${index + 1}`]"
+                        :class="{ 'bg-slate-800': index % 2 === 1 }">
+                            <td class="py-2 px-4 border-b border-addColor text-center">{{ index + 1 }}</td>
+                            <td class="py-2 px-4 border-b border-addColor text-center">{{ meal[`strIngredient${index + 1}`] }}</td>
+                            <td class="py-2 px-4 border-b border-addColor text-left">{{ meal[`strMeasure${index + 1}`] }}</td>
                         </tr>
                         </template>
                     </tbody>
