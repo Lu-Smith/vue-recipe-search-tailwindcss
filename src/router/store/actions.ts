93e463b4ae60fe7df1597d81cc7f679c7ae1a801
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-23 15:57:11 +0000

    Updated actions.js

--- src/router/store/actions.ts
+++ src/router/store/actions.ts
@@ -1,18 +1,8 @@
 import axiosClient from '../../axiosClient';
 import { Commit } from 'vuex';
 
-export function searchMeals({commit}: { commit: Commit }, letter: string) {
+export function searchMealsByLetter({commit}: { commit: Commit }, letter: string) {
     axiosClient.get(`search.php?f=${letter}`)
-    .then(({data}) => {
-        commit('setSearchedMeals', data.meals)
-    })
-    .catch((error) => {
-        console.error(error);
-    });
-}
-
-export function searchMealsByLetter({commit}: { commit: Commit }, keyword: string) {
-    axiosClient.get(`search.php?s=${keyword}`)
     .then(({data}) => {
         commit('setSearchedMealsByLetter', data.meals)
     })
@@ -21,10 +11,20 @@ export function searchMealsByLetter({commit}: { commit: Commit }, keyword: strin
     });
 }
 
-export function searchMealsByIngredient({commit}: { commit: Commit }, keyword: string) {
-    axiosClient.get(`search.php?s=${keyword}`)
+export function searchMealsByIngredient({commit}: { commit: Commit }, ingredient: string) {
+    axiosClient.get(`filter.php?i=${ingredient}`)
     .then(({data}) => {
         commit('setSearchedMealsByIngredient', data.meals)
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+export function searchMeals({commit}: { commit: Commit }, keyword: string) {
+    axiosClient.get(`search.php?s=${keyword}`)
+    .then(({data}) => {
+        commit('setSearchedMeals', data.meals)
     })
     .catch((error) => {
         console.error(error);
