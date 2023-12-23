5d61e93e36fac28509747ad704e897bd9a02e6c6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-12-23 17:36:06 +0000

    Updated Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
-      <h3 class="fixed bottom-4 right-4 w-ful text-sm mt-2">
-        Coded by 
-        <a 
-        href="https://www.lunasmithart.com/" 
-        target="_blank"
-        class="text-linksColor hover:text-orange-400">Luna Smith</a> -
-        <a 
-        href="https://github.com/Lu-Smith/vue-recipe-search-tailwindcss" 
-        target="_blank"
-        class="text-linksColor hover:text-orange-400">GitHub</a>.
-    </h3>
+  <h3 class="fixed bottom-4 right-4 w-ful text-sm mt-2">
+    Coded by 
+    <a 
+    href="https://www.lunasmithart.com/" 
+    target="_blank"
+    class="text-linksColor hover:text-orange-400">Luna Smith</a> -
+    <a 
+    href="https://github.com/Lu-Smith/vue-recipe-search-tailwindcss" 
+    target="_blank"
+    class="text-linksColor hover:text-orange-400">GitHub</a>.
+  </h3>
 </template>
