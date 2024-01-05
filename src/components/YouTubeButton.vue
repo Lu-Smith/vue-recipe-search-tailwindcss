f1e1e6ba2c4228caa3ae138fff1e508d530d601a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-05 18:56:46 +0000

    Updated YouTubeButton.vue

--- src/components/YouTubeButton.vue
+++ src/components/YouTubeButton.vue
@@ -1,6 +1,6 @@
 <template>
         <a 
-            :href="href" 
+            :href="props.href" 
             target="_blank" 
             class=" px-3 py-2 rounded border border-textColor
             hover:bg-red-600 transition-colors w-full text-center">
@@ -10,6 +10,6 @@
 
 <script lang="ts" setup>
     const props = defineProps({
-        href: String
+        href: String 
     })
 </script>
